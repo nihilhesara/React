3aa39570858cd66412c49f5c7860b23ade3ac2a3
Your Name <nihilhesa@gmail.com>
2025-05-15 14:04:15 +0530

    Basics in react

---
+++ react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header"
+import Footer from "./Footer" 
+import Food from "./Food"
+
+function App() {
+  return(
+    // we can only return a single tag in a return statement so we put a blank tag to make it a single tag
+    <>
+    <Header></Header>
+    <Food></Food>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App
