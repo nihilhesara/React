a92eda80f93fae529fffc7e4982f20fe2feee183
Your Name <nihilhesa@gmail.com>
2025-05-23 14:37:19 +0530

    on chnage event

---
+++ onChange-event/src/App.jsx
@@ -0,0 +1,16 @@
+import MyComponent from "./MyComponent";
+
+function App() {
+  return(
+    <div>
+      <MyComponent />
+    </div>
+  );
+}
+
+
+export default App
+
+// onChange = event handler used primarily with form elements 
+// ex:- <input> <textarea> <select> <radio>
+// triggers a function every time the value of the input changes.
